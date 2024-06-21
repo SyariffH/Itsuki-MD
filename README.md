6c319e9c11f9077eb091fd1f4d33ce56c333de06
Syll Takamiya <144473220+SyariffH@users.noreply.github.com>
2024-06-22 02:47:58 +0700

    Syll

---
+++ README.md
@@ -0,0 +1,56 @@
+
+### WHATSAPP BOT BASE
+<img src="https://telegra.ph/file/e5ecb1a742e5a79e25951.jpg" alt="Hyzer Multi Device" width="300" />
+
+[![JavaScript](https://img.shields.io/badge/JavaScript-d6cc0f?style=for-the-badge&logo=javascript&logoColor=white)](https://javascript.com) [![NodeJS](https://img.shields.io/badge/Node.js-43853D?style=for-the-badge&logo=node.js&logoColor=white)](https://nodejs.org/)
+
+## SETTINGS
+
+Isi semua informasi bot di file [`settings.json`](https://github.com/SyariffH/Itsuki-MD/blob/main/global/settings.json)<br />
+
+## ITSUKI BOT ROOM
+[![WHATSAPP](https://img.shields.io/badge/WhatsApp%20Group-25D366?style=for-the-badge&logo=whatsapp&logoColor=white)](https://chat.whatsapp.com/IxBejqgYlXKENKPJsF7EOP) 
+
+## TERMUX USER
+```bash
+$ pkg upgrade && pkg update
+$ pkg install nodejs
+$ pkg install ffmpeg
+$ pkg install libwebp
+$ pkg install imagemagick
+$ pkg install git
+$ pkg install yarn
+$ gitclone https://github.com/SyariffH/Itsuki-MD
+$ cd Itsuki-MD
+$ yarn
+$ rm -rf global/session.json
+$ node .
+```
+
+## CONNECT
+
+Jika sudah ada session, isi di file [`session.json`](https://github.com/SyariffH/Itsuki-MD/blob/main/system/connect/session.json)<br />
+
+## CONTACTS ME
+
+Untuk request atau report bug bisa chat saya disini [Whatsapp](https://wa.me/6287768886148)
+
+## Highlights
+
+-   [x] Simple Penggunaan,
+-   [x] Mudah digunakan,
+-   [x] Stabil,
+-   [x] Dan ringan
+
+## Thanks To
+[![Fatih Arridho](https://github.com/FatihArridho.png?size=100)](https://github.com/FatihArridho)
+[![Ilman](https://github.com/ilmanhdyt.png?size=100)](https://github.com/ilmanhdyt)
+[![Zackmans](https://github.com/zackmans.png?size=100)](https://github.com/zackmans)
+[![DikaArdnt](https://github.com/DikaArdnt.png?size=100)](https://github.com/DikaArdnt)
+[![Fadly Id](https://github.com/xdlyy404.png?size=100)](https://github.com/xdlyy404)
+[![X-Far](https://github.com/xfar05.png?size=100)](https://github.com/xfar05)
+[![Caliph](https://github.com/caliphdev.png?size=100)](https://github.com/caliphdev)
+[![BochilScraper](https://github.com/bochilteam.png?size=100)](https://github.com/bochilteam)
+[![Sad Team](https://github.com/sadteams.png?size=100)](https://github.com/sadteams)
+[![Hyzerr](https://github.com/Alusius.png?size=100)](https://github.com/Alusius)
+[![Syll](https://github.com/SyariffH.png?size=100)](https://github.com/SyariffH)
---
+++ hyzer.js
@@ -0,0 +1,225 @@
+(async () => {
+const {
+useMultiFileAuthState,
+DisconnectReason
+} = require('baileys')
+const { generate } = require('qrcode-terminal')
+const WebSocket = require('ws')
+const path = require('path')
+const fs = require('fs')
+const yargs = require('yargs/yargs')
+const cp = require('child_process')
+const _ = require('lodash')
+const syntaxerror = require('syntax-error')
+const P = require('pino')
+const os = require('os')
+const chalk = require('chalk')
+const sock = require('./system/socket')
+var low
+try {
+low = require('lowdb')
+} catch (e) {
+low = require('./system/database/LowDB')
+}
+const { Low, JSONFile } = low
+const mongoDB = require('./system/database/mongoDB')
+
+global.timestamp = {
+start: new Date
+}
+global.Func = require('./system/function.js')
+global.Scraper = require('./system/scraper.js')
+
+const PORT = process.env.PORT || 3000
+
+global.opts = new Object(yargs(process.argv.slice(2)).exitProcess(false).parse())
+global.prefix = new RegExp('^[' + (opts['prefix'] || '‎xzXZ/i!#$%+£¢€¥^°=¶∆×÷π√✓©®:;?&.\\-').replace(/[|\\{}()[\]^$+*?.\-\^]/g, '\\$&') + ']')
+
+global.db = new Low(
+/https?:\/\//.test(opts['db'] || '') ?
+new cloudDBAdapter(opts['db']) : /mongodb/.test(opts['db']) ?
+new mongoDB(opts['db']) :
+new JSONFile(`./system/database/database.json`)
+)
+global.loadDatabase = async function loadDatabase() {
+if (global.db.READ) return new Promise((resolve) => setInterval(function () { (!global.db.READ ? (clearInterval(this), resolve(global.db.data == null ? global.loadDatabase() : global.db.data)) : null) }, 1 * 1000))
+  if (global.db.data !== null) return
+global.db.READ = true
+await global.db.read()
+global.db.READ = false
+global.db.data = {
+users: {},
+chats: {},
+database: {},
+game: {},
+settings: {},
+others: {},
+sticker: {},
+anonymous: {},
+...(global.db.data || {})
+}
+
+global.db.chain = _.chain(global.db.data)
+}
+loadDatabase()
+
+global.authFile = './kanaeru'
+global.isInit = !fs.existsSync(authFile)
+global.Info = JSON.parse(fs.readFileSync('./global/settings.json'))
+var { state, saveCreds } = await useMultiFileAuthState(authFile)
+
+const connectionOptions = {
+  printQRInTerminal: true,
+  auth: state,
+  logger: P({ level: 'silent' }),
+  version: [2, 2204, 13]
+}
+
+global.client = sock.makeWASocket(connectionOptions)
+
+if (!opts['test']) {
+if (global.db) setInterval(async () => {
+if (global.db.data) await global.db.write()
+if (opts['autocleartmp'] && (global.support || {}).find) (tmp = [os.tmpdir(), 'tmp'], tmp.forEach(filename => cp.spawn('find', [filename, '-amin', '3', '-type', 'f', '-delete'])))
+}, 30 * 1000)
+}
+
+if (opts['big-qr'] || opts['server']) client.ev.on('qr', qr => generate(qr, { small: false }))
+if (opts['server']) require('./system/server')(global.client, PORT)
+
+async function connectionUpdate(update) {
+const { connection, lastDisconnect } = update
+global.timestamp.connect = new Date
+//if (lastDisconnect && lastDisconnect.error && lastDisconnect.error.output && lastDisconnect.error.output.statusCode !== DisconnectReason.loggedOut && client.ws.readyState !== WebSocket.CONNECTING) {
+//console.log(global.reloadHandler(true))
+//}
+if (global.db.data == null) await loadDatabase()
+console.log(update)
+//console.log(JSON.stringify(update, null, 4))
+}
+
+const imports = (path) => {
+path = require.resolve(path)
+let modules, retry = 0
+do {
+if (path in require.cache) delete require.cache[path]
+modules = require(path)
+retry++
+} while ((!modules || (Array.isArray(modules) || modules instanceof String) ? !(modules || []).length : typeof modules == 'object' && !Buffer.isBuffer(modules) ? !(Object.keys(modules || {})).length : true) && retry <= 10)
+return modules
+}
+let isInit = true
+global.reloadHandler = function (restatClient) {
+let handler = imports('./system/validation')
+if (restatClient) {
+try { global.client.ws.close() } catch { }
+global.client = {
+...global.client, ...sock.makeWASocket(connectionOptions)
+}
+}
+if (!isInit) {
+client.ev.off('messages.upsert', client.handler)
+client.ev.off('group-participants.update', client.participantsUpdate)
+client.ev.off('connection.update', client.connectionUpdate)    
+}
+
+client.welcome = 'Welcome @user To Group @subject'
+client.bye = '@user Leaving Group @subject :('
+client.spromote = '@user sekarang admin!'
+client.sdemote = '@user sekarang bukan admin!'
+client.handler = handler.handler.bind(client)  
+client.handler = handler.handler.bind(client)
+client.connectionUpdate = connectionUpdate.bind(client)
+client.participantsUpdate = handler.participantsUpdate.bind(client)
+
+client.ev.on('messages.upsert', client.handler)  
+client.ev.on('connection.update', client.connectionUpdate)
+client.ev.on('group-participants.update', client.participantsUpdate)
+isInit = false
+return true
+}
+
+global.reloadFile = (file, options = {}) => {
+nocache(file, module => {
+//console.log(chalk.keyword('yellow')(`File "${file}" telah diupdate, me-restart bot.`))
+process.send("reset")
+})
+}
+function nocache(module, cb = () => {}) {
+fs.watchFile(require.resolve(module), async () => {
+await uncache(require.resolve(module))
+cb(module)
+})
+}
+
+function uncache(module = '.') {
+return new Promise((resolve, reject) => {
+try {
+delete require.cache[require.resolve(module)]
+resolve()
+} catch (e) {
+reject(e)
+}
+})
+} // Credits By Hyzer Official, Don't Delete this
+global.plugins = {} 
+let pluginFolder = path.join(__dirname, "./plugins")
+let fitur = fs.readdirSync(pluginFolder)
+fitur.forEach(async (res) => {
+const hijer = fs.readdirSync(`${pluginFolder}/${res}`).filter((file) => file.endsWith(".js"))
+for (let filename of hijer ) {
+try {
+global.plugins[filename] = require(`${pluginFolder}/${res}/${filename}`)
+require("delay")(100)
+global.reloadFile(`./plugins/${res}/${filename}`)
+} catch (e) {
+client.logger.error(e)
+delete global.plugins[filename]
+}
+}
+})
+
+global.reloadHandler()
+
+// Quick Test
+async function _quickTest() {
+let test = await Promise.all([
+cp.spawn('ffmpeg'),
+cp.spawn('ffprobe'),
+cp.spawn('ffmpeg', ['-hide_banner', '-loglevel', 'error', '-filter_complex', 'color', '-frames:v', '1', '-f', 'webp', '-']),
+cp.spawn('convert'),
+cp.spawn('magick'),
+cp.spawn('gm'),
+cp.spawn('find', ['--version'])
+].map(p => {
+return Promise.race([
+new Promise(resolve => {
+p.on('close', code => {
+resolve(code !== 127)
+})
+}),
+new Promise(resolve => {
+p.on('error', _ => resolve(false))
+})
+])
+}))
+let [ffmpeg, ffprobe, ffmpegWebp, convert, magick, gm, find] = test
+//console.log(test)
+let s = global.support = {
+ffmpeg,
+ffprobe,
+ffmpegWebp,
+convert,
+magick,
+gm,
+find
+}
+Object.freeze(global.support)
+
+if (!s.ffmpeg) client.logger.warn('🚩 Please install ffmpeg for sending videos (pkg install ffmpeg)')
+if (s.ffmpeg && !s.ffmpegWebp) client.logger.warn('🚩 Stickers may not animated without libwebp on ffmpeg (--enable-ibwebp while compiling ffmpeg)')
+if (!s.convert && !s.magick && !s.gm) client.logger.warn('🚩 Stickers may not work without imagemagick if libwebp on ffmpeg doesnt installed (pkg install imagemagick)')
+}
+
+_quickTest()
+})()
