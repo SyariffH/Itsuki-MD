fcdde0cc1abb18a6a75ceb53ae5061846afee27c
Syll Takamiya <144473220+SyariffH@users.noreply.github.com>
2024-06-22 02:57:32 +0700

    Syll

--- plugins/anime/loli.js
+++ plugins/anime/loli.js
@@ -1,1 +1,16 @@
+var fetch = require('node-fetch')
 
+var handler = async(msg, { 
+client
+}) => {
+var buton = await Func.satuButton(`.loli`, 'NEXT')
+var imge = await fetch('https://raw.githubusercontent.com/Hyzerr/Database/master/Database/Anime/loli.json')
+var img = await imge.json()
+var res = Func.pickRandom(img)
+client.sendImage(msg.from, res, 'Random anime loli.', msg, { isUrl: true, buttons: buton, headerType: 5, footer: wm })
+}
+handler.help = ['loli']
+handler.tags = ['anime']
+handler.command = /^(loli)$/i
+
+module.exports = handler
